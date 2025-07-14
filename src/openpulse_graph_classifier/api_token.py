5911258e82b03a0c248e4c728437fd942ee05c29
Laure Vancau <laure.vancauwenberghe@epfl.ch>
2025-07-14 10:26:09 +0200

    fix: remove fastapi workflow and set api token

--- src/openpulse_graph_classifier/api_token.py
+++ src/openpulse_graph_classifier/api_token.py
@@ -6,11 +6,13 @@ import requests
 load_dotenv()
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
+AIRFLOW_USERNAME = os.getenv("AIRFLOW_USERNAME")
+AIRFLOW_PASSWORD = os.getenv("AIRFLOW_PASSWORD")
 
 INFERENCE_URL = os.getenv("INFERENCE_URL")
 NEO4J_DATABASE = os.getenv("NEO4J_DATABASE")
 
-payload = {"service": "airflow", "exp": 9999999999}
+payload = {"username": AIRFLOW_USERNAME, "password": AIRFLOW_PASSWORD}
 
 token = jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
