b3c2967a23b4340c5afcf0fe1c613e238e52f0ba
Laure Vancau <laure.vancauwenberghe@epfl.ch>
2025-10-03 15:04:54 +0200

    fix: typo

--- src/classifier/huggingface/hf_upload_model.py
+++ src/classifier/huggingface/hf_upload_model.py
@@ -4,16 +4,14 @@ import os
 
 load_dotenv()
 
+
 def upload_model_to_huggingface():
     HF_TOKEN = os.environ.get("HUGGINGFACE_TOKEN")
     HF_ENDPOINT = os.environ.get("HUGGINGFACE_ENDPOINT")
     REPO_ID = os.environ.get("HUGGINGFACE_REPO")
     MODEL_PATH = os.environ.get("HUGGINGFACE_MODEL_PATH")
 
-    api = HfApi(
-        endpoint=HF_ENDPOINT
-        token=HF_TOKEN
-    )
+    api = HfApi(endpoint=HF_ENDPOINT, token=HF_TOKEN)
     api.upload_file(
         path_or_fileobj="classifier/models/supervised_hetero.pt",
         path_in_repo=MODEL_PATH,
